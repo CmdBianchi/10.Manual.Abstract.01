44ff1b46d50bd2d904c4c0b0d355322d11c38e09
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-13 15:15:56 -0300

    Adicionar arquivos de projeto.

---
+++ 10.Manual.Abstract/Entitites/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entitites;
+namespace Entitites {
+    abstract class Account { // ---- > Assim que se estancia uma classe abstrata.
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount) {
+            Balance -= amount + 5.0;
+        }
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+    }
+}
---
+++ 10.Manual.Abstract/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Entitites;
+
+namespace _10.Manual.Abstract {
+    class Program {
+        static void Main(string[] args) {
+            List<Account> list = new List<Account>();
+            /* Metodo Abstrato não pode ser instaciado, deve - se estaciar direto os submetodos. 
+             Assim devese usar as subclasses diretamente, o METODO ABSTRAT não permite. */
+
+            list.Add(new SavingsAccount(1001, "Alex", 500.00, 0.01)); /
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            list.Add(new SavingsAccount(1004, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+
+            double sum = 0.0;
+            foreach (Account acc in list) {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("Total balance: " + sum.ToString("F2"));
+
+            foreach (Account acc in list) {
+                acc.Withdraw(10.0);
+            }
+            foreach (Account acc in list) {
+                Console.WriteLine("Updated balance for account "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2"));
+            }
+        }
+    }
+}
